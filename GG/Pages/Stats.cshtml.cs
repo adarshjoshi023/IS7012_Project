0a723d3046e309ae96491d8b3fc998b57570e4de
adarshjoshi023 <adarshjoshi.kodapganti5@gmail.com>
2022-10-05 05:30:15 -0400

    Final Project

---
+++ GG/Pages/Stats.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using GG.Data;
+using GG.Models;
+
+namespace GG.Pages
+{
+    public class StatsModel : PageModel
+    {
+        private readonly GG.Data.ApplicationDbContext _context;
+
+        public StatsModel(GG.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Sport> Sport { get;set; } = default!;
+
+       
+        public async Task OnGetAsync( )
+        {
+           
+            if (_context.Sport != null)
+            {
+                Sport = await _context.Sport.Include(c=>c.players).Include(c=>c.playgrounds).ToListAsync();
+            }
+        }
+    }
+}
